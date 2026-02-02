570b3bdb7cf31d244118182c0b5ef546fdd4f1bf
aditbansal1 <aditbans@usc.edu>
2026-02-01 23:55:16 -0600

    Final tcp_client.py
    Change ip address in line 14 as needed

--- tcp_client.py
+++ tcp_client.py
@@ -9,8 +9,22 @@ import socket
 
 def main():
     # TODO: Create a socket and connect it to the server at the designated IP and port
+    client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     # TODO: Get user input and send it to the server using your TCP socket
+    server_address = ('172.20.10.2', 12345) 
+    client_socket.connect(server_address)
     # TODO: Receive a response from the server and close the TCP connection
+    try:
+        message = input("Enter a message to send: ")    #getting user input
+        
+        client_socket.sendall(message.encode())             #sending data
+        
+        data = client_socket.recv(256)                        #receive data
+        print(f"Received from server: {data.decode()}")
+        
+    finally:
+        
+        client_socket.close()                   #close the connection
     pass
 
 
